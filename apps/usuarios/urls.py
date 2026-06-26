b3aea78b15318a3bf19415f1daf24c8d5851c63f
Ckelyson <contatockelyson@gmail.com>
2026-06-26 00:14:12 -0300

    Remove cadastro publico

--- apps/usuarios/urls.py
+++ apps/usuarios/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from apps.usuarios.views import login, cadastro, logout
+from apps.usuarios.views import login, logout
 
 urlpatterns = [
     path('login', login, name='login'),
-    path('cadastro', cadastro, name='cadastro'),
+    
     path('logout', logout, name='logout'),
 ]
