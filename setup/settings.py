dde6fde27c63b78c66dfe2e920420a446fe5bdce
Ckelyson <contatockelyson@gmail.com>
2026-07-23 00:50:06 -0300

    subindo

--- setup/settings.py
+++ setup/settings.py
@@ -127,13 +127,23 @@ WSGI_APPLICATION = "setup.wsgi.application"
 # BANCO DE DADOS
 # ======================================================
 
-DATABASES = {
-    "default": dj_database_url.config(
-        default=os.getenv("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=True,
-    )
-}
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=True,
+        )
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 # ======================================================
 # VALIDAÇÃO DE SENHAS
